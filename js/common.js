d6d6a6adf57d1085adf6c6759c663362bb2643ca
MinozAmy <245367650@qq.com>
2017-10-14 18:55:02 +0800

    公共部分初版

---
+++ js/common.js
@@ -0,0 +1,17 @@
+
+	jQuery(function($){
+		// 初始化
+		$(".row .col-xs-3 ul li").eq(1).addClass('active');
+		$('.col-md-9 .list-group .list-group-item.active').text("欢迎进入销售管理系统");
+		// tab切换
+		$(".row .col-xs-3 ul li").on('click',function(){
+			$(this).addClass('active').siblings().removeClass('active');
+			//设置内容标题
+				var title = $(this).children('a').text();
+				$('.col-md-9 .list-group .list-group-item.active').text("欢迎进入"+title+"系统");
+
+			//更改内容
+
+		})
+
+	})

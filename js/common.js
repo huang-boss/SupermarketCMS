0c0b3675d369ce01fb208116de42517741a0b52d
MinozAmy <245367650@qq.com>
2017-10-17 16:09:32 +0800

    商品管理

--- js/common.js
+++ js/common.js
@@ -1,14 +1,14 @@
 
 	jQuery(function($){
 		// 初始化
-		$(".row .col-xs-3 ul li").eq(1).addClass('active');
-		$('.col-md-9 .list-group .list-group-item.active').text("欢迎进入销售管理系统");
+		$(".row .col-xs-2 ul li").eq(1).addClass('active');
+		$('.col-md-10 .list-group .list-group-item.active').text("欢迎进入销售管理系统");
 		// tab切换
-		$(".row .col-xs-3 ul li").on('click',function(){
+		$(".row .col-xs-2 ul li").on('click',function(){
 			$(this).addClass('active').siblings().removeClass('active');
 			//设置内容标题
 				var title = $(this).children('a').text();
-				$('.col-md-9 .list-group .list-group-item.active').text("欢迎进入"+title+"系统");
+				$('.col-md-10 .list-group .list-group-item.active').text("欢迎进入"+title+"系统");
 
 			//更改内容
 
--- router/DBHelper.js
+++ router/DBHelper.js
@@ -1,8 +1,7 @@
 var mongodb = require('mongodb');
-var dbServer = new mongodb.Server('localhost', 27017);
+var dbServer = new mongodb.Server('10.3.131.15 ', 27017);
 var db = new mongodb.Db('sm', dbServer);
 var apiResult = require('./ApiResult.js');
-
 var obj = {
 	insert: function(_collection, _data, _callback){
 		db.open(function(error, db){
@@ -21,9 +20,73 @@ var obj = {
 			})
 		})
 	},
-	find: function(collection, data, callback){},
-	delete: function(collection, data, callback){},
-	update: function(collection, data, callback){}
+	select: function(_collection, _data, _callback){
+		db.open(function(error, db){
+			if(error){
+				_callback({status: false, message: error});
+			} else {
+				db.collection(_collection, function(error, collection){
+					if(error){
+						_callback({status: false, message: error});
+					} else {
+						collection.find(_condition || {}).toAarry(function(error, dataset){
+							if(error){
+								_callback({status: false, message: error});
+							} else {
+								_callback({status: true, data: dataset});
+							}
+						})
+					}
+				})
+			}
+		})
+	},
+	delete: function(collection, data, callback){
+		db.open(function(error, db){
+			if(error){
+				_callback({status: false, message: error});
+			} else {
+				db.collection(_collection, function(error, collection){
+					if(error){
+						_callback({status: false, message: error});
+					} else {
+                       collection.remove({title:'hello'},{safe:true},function(error,result){
+                          console.log(result);
+							if(error){
+								_callback({status: false, message: error});
+							} else {
+								_callback({status: true, data: dataset});
+							}
+						})
+					}
+				})
+			}
+		})  
+
+	},
+	
+	// update: function(collection, data, callback){
+	// 	db.open(function(error, db){
+	// 		if(error){
+	// 			_callback({status: false, message: error});
+	// 		} else {
+	// 			db.collection(_collection, function(error, collection){
+	// 				if(error){
+	// 					_callback({status: false, message: error});
+	// 				} else {
+	// 					collection.update({title:'hello'}, {$set:{number:3}}, {safe:true}, function(err, result){
+	// 					    console.log(result);
+	// 						if(error){
+	// 							_callback({status: false, message: error});
+	// 						} else {
+	// 							_callback({status: true, data: dataset});
+	// 						}
+	// 					})
+	// 				}
+	// 			})
+	// 		}
+	// 	})  
+	// }
 }
 
 module.exports = obj;
